f3de689b94294f167e7fe957a7b938dbe669daa1
maxivaldez <117707213+maxivaldez@users.noreply.github.com>
2022-11-22 19:56:51 -0300

    Create menu2.js

---
+++ js/menu2.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector('nav');
+let botonAbrir = document.querySelector('.menu-abrir');
+let botonCerrar = document.querySelector('.menu-cerrar');
+
+function abrirMenu () {
+	menu.classList.add('activo');
+}
+
+function cerrarMenu () {
+	menu.classList.remove('activo');
+}
+
+botonAbrir.addEventListener('click', abrirMenu);
+botonCerrar.addEventListener('click', cerrarMenu);
+
+
+let mediaqueryList = window.matchMedia("(min-width: 1024px)");
+
+if(!mediaqueryList.matches) {
+	$(document).ready(function(){
+		$('.sub-btn').click(function(){
+			$(this).next('.sub-menu').slideToggle();
+		});
+	});
+}
